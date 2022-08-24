66b31c2f6697fc8d9f6831016817cf2aebacc5ea
me <2072811261@qq.com>
2022-08-24 16:23:00 +0800

    init

--- HAL_Driver/HAL_Driver.c
+++ HAL_Driver/HAL_Driver.c
@@ -1,2 +1,85 @@
 #include "HAL_Driver.h"
 
+
+static uint32_t TickUs = 0;
+static uint32_t TickMs = 0;
+static uint64_t RunTickMs = 0;
+
+void  SysTick_Init (uint32_t ticks)
+{
+	TickMs = ticks / 8 / 1000;
+	TickUs = TickMs / 1000;
+    SysTick->CTRL &= SysTick_CLKSource_HCLK_Div8;
+    SysTick->LOAD = TickMs - 1;
+    SysTick->VAL  = 0;
+    NVIC_SetPriority(SysTick_IRQn, 0xff);
+    SysTick->CTRL |= SysTick_CTRL_TICKINT_Msk | SysTick_CTRL_ENABLE_Msk;
+    
+}
+
+/** 
+ * @brief: 
+ * @param uint32_t us
+ * @retval: 
+ */
+void Delay_us(uint32_t us)
+{
+    uint32_t ticksNeed = us * TickUs;
+	uint32_t ticksCount = 0;
+	uint32_t oldValue = SysTick->VAL;
+	uint32_t newValue;
+	while(1) 
+	{
+		newValue = SysTick->VAL;
+		if(newValue != oldValue)
+		{
+			if(newValue < oldValue) {
+				ticksCount += (oldValue-newValue);
+			}
+			else {
+				ticksCount += (TickMs-newValue+oldValue);
+			}
+			oldValue = newValue;
+			if(ticksCount >= ticksNeed) {
+				break;
+			}
+		}
+	}
+}
+
+/** 
+ * @brief: 
+ * @param uint32_t ms
+ * @retval: 
+ */
+void Delay_ms(uint32_t ms)
+{
+	for(uint32_t i = 0; i < ms; i++)
+		Delay_us(1000);
+}
+
+uint8_t TimeBaseFlag(TimeBaseFlag_TypeDef * pTimeBaseFlag)
+{
+	if(pTimeBaseFlag->TriggerTime < RunTickMs)
+	{
+		if(++pTimeBaseFlag->TriggerCnt >= pTimeBaseFlag->TriggerCntMax)
+		{
+			pTimeBaseFlag->TriggerCnt = 0;
+			pTimeBaseFlag->TriggerTime = RunTickMs + pTimeBaseFlag->DelayMs;
+			return 1;
+		}
+		return 1;
+	}
+	return 0;
+}
+
+/**
+  * @brief  This function handles SysTick Handler.
+  * @param  None
+  * @retval None
+  */
+void SysTick_Handler(void)
+{
+	RunTickMs++;
+  	lv_tick_inc(1);
+}
--- HAL_Driver/HAL_Driver.h
+++ HAL_Driver/HAL_Driver.h
@@ -1,12 +1,14 @@
 /** 
  * @Author: 时间世纪
- * @Date: 2022-08-15 09:31:54
+ * @Date: 2021-08-05 09:31:54
  * @Description: 硬件驱动层
  */
 #ifndef _HAL_DRIVER_H_
 #define _HAL_DRIVER_H_
 
 #include "stm32f4xx.h"
+#include "ITM.h"
+#include "lvgl.h"
 
 //包含控制IO口的必要条件
 typedef struct 
@@ -16,41 +18,37 @@ typedef struct
 	uint16_t		GPIO_PIN;//0~15
 }HAL_GPIO_TypeDef;
 
+
+typedef struct
+{
+    uint32_t DelayMs;
+    uint8_t TriggerCntMax;
+    uint64_t TriggerTime;
+    uint8_t TriggerCnt;
+}TimeBaseFlag_TypeDef;
+
+void  SysTick_Init (uint32_t cnts);
+
 /** 
  * @brief 微妙级延时
  * @param uint32_t t
  * @retval 
  */
-__STATIC_FORCEINLINE void Delay_us(uint32_t t)
-{
-    //delay_us(t);
-	for(uint32_t i = 0; i < t; i++)
-	{
-		for (uint32_t j = 0; j < 10; j++)
-		{
-			__nop();
-		}
-	}
-}
+extern void Delay_us(uint32_t us);
 
 /** 
  * @brief 毫妙级延时
  * @param uint32_t t
  * @retval 
  */
-__STATIC_FORCEINLINE void Delay_ms(uint32_t t)
-{
-    //delay_us(t);
-	for(uint32_t i = 0; i < t; i++)
-    {
-        Delay_us(1000);
-    }
-}
+extern void Delay_ms(uint32_t ms);
 
 __STATIC_FORCEINLINE void HAL_GPIO_Init(HAL_GPIO_TypeDef * pGPIO)
 {
     RCC->AHB1ENR |= pGPIO->GPIO_CLK;
 }
+
+uint8_t TimeBaseFlag(TimeBaseFlag_TypeDef * pTimeBaseFlag);
 
 /** 
  * @brief 设置端口电平
